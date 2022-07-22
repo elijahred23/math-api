ffa751922a36dbab7a6175207f117a71435118d5
Elijah <elijahred23@gmail.com>
2022-07-22 06:17:34 +0100

    Added math api

--- index.js
+++ index.js
@@ -9,6 +9,30 @@ app.get("/", (req,res)=>{
 	res.render("index");
 });
 
+app.get("/math", (req,res)=>{
+	let query = res.query;
+	let num1 = parseFloat(query.num1);
+	let num2 = parseFloat(query.num2);
+	let operator = query.operator;
+	let result;
+	switch(operator){
+		case '+': result = num1+num2;
+			break;
+		case '-': result = num1-num2;
+			break;
+		case '*': result = num1*num2;
+			break;
+		case '/': result = num1/num2;
+			break;
+	}
+	res.send({
+		num1: num1,
+		num2: num2,
+		operator: operator,
+		result: result
+	});
+})
+
 app.listen(port, () =>{
 
 	console.log(`Listening on port ${port}`);
